ae185d70fb82911fc7cd421fe595017c802c22da
Eol <hslphst@gmail.com>
2020-10-04 16:41:15 +0800

    yarn2 env init

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  globals: {
+    window: true,
+    module: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    indent: [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'never',
+      {
+        beforeStatementContinuationChars: 'any'
+      }
+    ]
+  }
+}
